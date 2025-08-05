662362efb6085ed166a4b030b023783f6db0d19b
Markin <Markin-work@protonmail.com>
2025-08-05 05:08:41 -0300

    feat: add default headers for metrics

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
 
+const userAgent = navigator.userAgent;
 export const api = axios.create({
-	baseURL: import.meta.env.VITE_URL_API_DEV,
+	baseURL: import.meta.env.VITE_API_URL,
 	timeout: 20000,
+	headers: {
+		"x-app-version": import.meta.env.VITE_WEB_VERSION,
+		"x-device-platform": "WEB",
+		"x-platform-version": userAgent,
+	},
 });
 
 export const publicApi = axios.create({
-	baseURL: import.meta.env.VITE_URL_API_DEV,
+	baseURL: import.meta.env.VITE_API_URL,
 	timeout: 10000,
+	headers: {
+		"X-app-version": import.meta.env.VITE_WEB_VERSION,
+		"X-device-platform": "WEB",
+		"X-platform-version": userAgent,
+	},
 });

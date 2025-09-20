778beee700f51d6cd8b9605299f14e603c4b927c
Markin <Markin-work@protonmail.com>
2025-09-20 03:47:49 -0300

    chore: change env files

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -1,12 +1,11 @@
 import { z } from "zod/v4";
 
 const envSchema = z.object({
-		VITE_IS_DEVELOPMENT: z.string().transform((val) => val === "true"),
-    VITE_DEV_MODE: z.string().transform((val) => val === "true"),
-		VITE_URL_API: z.url(),
-		VITE_URL_API_DEV: z.url(),
-		VITE_DISCORD_ERROR_WEBHOOK: z.url(),
-    VITE_WEB_VERSION: z.string().min(1),
+	VITE_IS_DEVELOPMENT: z.string().transform((val) => val === "true"),
+	VITE_DEV_MODE: z.string().transform((val) => val === "true"),
+	VITE_URL_API: z.url(),
+	VITE_URL_API_DEV: z.url(),
+	VITE_WEB_VERSION: z.string().min(1),
 });
 export function checkEnv() {
 	const { success } = envSchema.safeParse(import.meta.env);

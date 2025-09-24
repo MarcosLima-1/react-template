0c9e85ca708d6a20742ec0443c89c4b31993c490
Markin <Markin-work@protonmail.com>
2025-09-24 01:20:49 -0300

    chore: update storage theme system

--- src/modules/theme/storage/theme.ts
+++ src/modules/theme/storage/theme.ts
@@ -1,14 +1,22 @@
-import type { ThemesClass } from "../constants/themes.ts";
+import type { ThemeKeys } from "@/modules/theme/constants/themes";
 
 const THEME_KEY = "theme";
 
-export function saveThemeInStorage(theme: ThemesClass) {
+export function saveThemeInStorage(theme: ThemeKeys) {
 	localStorage.setItem(THEME_KEY, theme);
 }
 
-export function getStorageTheme(): ThemesClass | undefined {
-	const theme = localStorage.getItem(THEME_KEY) as ThemesClass | undefined;
-	return theme;
+export function getStorageTheme(): ThemeKeys {
+	const storedTheme = localStorage.getItem(THEME_KEY) as ThemeKeys | undefined;
+	if (storedTheme) {
+		return storedTheme;
+	}
+
+	if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+		return "dark";
+	}
+
+	return "light";
 }
 
 export function deleteStorageTheme() {

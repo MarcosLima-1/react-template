ace728d55fb120f62c309689c383c8838331012b
Markin <Markin-work@protonmail.com>
2025-10-31 17:17:23 -0300

    feat: add bytes format

--- src/types/tanstack-meta.ts
+++
@@ -1,8 +0,0 @@
-type methods = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
-export interface TanstackMetaTags {
-	method: methods[];
-	desc: string;
-	errorMessage: string;
-	successMessage?: string;
-  invalidateQueries?: unknown[]
-}

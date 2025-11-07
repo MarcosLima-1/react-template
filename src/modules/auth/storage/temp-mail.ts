e6ec1725ef6d19d9b1fcb38f7d8541ab3e74151f
Markin <Markin-work@protonmail.com>
2025-11-07 17:42:28 -0300

    feat: add validation to tempMail

--- src/modules/auth/storage/temp-mail.ts
+++ src/modules/auth/storage/temp-mail.ts
@@ -1,10 +1,14 @@
-import { STORAGE_KEYS } from "../core/storage";
+import { STORAGE_KEYS } from "@/modules/auth/core/storage";
+import { validateEmail } from "@/utils/regex/validate-email";
 
 export function saveTempMailInStorage(email: string) {
 	localStorage.setItem(STORAGE_KEYS.TEMP_MAIL, email);
 }
 export function getStorageTempMail() {
 	const email = localStorage.getItem(STORAGE_KEYS.TEMP_MAIL);
+	const { success } = validateEmail.safeParse(email);
+
+	if (!success) return null;
 	return email;
 }
 

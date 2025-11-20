32bfb09e7aa5c9c87caef9ba98304f47b32eba36
Markin <Markin-work@protonmail.com>
2025-11-20 19:50:19 -0300

    fix: default value on select field

--- src/modules/form/form-fields/select-field.tsx
+++ src/modules/form/form-fields/select-field.tsx
@@ -2,24 +2,24 @@ import type { LucideIcon } from "lucide-react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useFieldContext } from "@/modules/form/app-form";
 
-export interface SelectOption {
-	value: string;
+export interface SelectOption<T extends string = string> {
+	value: T;
 	label: string;
 	icon?: LucideIcon;
 }
 
-interface SelectFieldProps {
-	options: SelectOption[];
-	defaultValue?: string;
+interface SelectFieldProps<T extends string> {
+	options: readonly SelectOption<T>[];
+	defaultValue?: T;
 	placeholder?: string;
 }
-
-export function SelectField({ options, placeholder, defaultValue }: SelectFieldProps) {
-	const field = useFieldContext();
+export function SelectField<T extends string>({ options, placeholder, defaultValue }: SelectFieldProps<T>) {
+	const field = useFieldContext<string>();
+	const value = field.state.value;
 
 	return (
-		<Select defaultValue={defaultValue} onValueChange={field.handleChange}>
-			<SelectTrigger className="w-[180px]" id={field.name}>
+		<Select value={value} defaultValue={defaultValue} onValueChange={field.handleChange}>
+			<SelectTrigger id={field.name} className="w-[180px]">
 				<SelectValue placeholder={placeholder} />
 			</SelectTrigger>
 			<SelectContent>
